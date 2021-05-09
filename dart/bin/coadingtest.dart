df7fe52b16ae04d48b2c953d08f96e9fb0866add
HoduGwaja <honeybutter9250@gmail.com>
2021-05-09 23:59:26 +0900

    [commit | 2021.05.09 | added dart]

---
+++ dart/bin/coadingtest.dart
@@ -0,0 +1,5 @@
+import 'package:coadingtest/coadingtest.dart' as coadingtest;
+
+void main(List<String> arguments) {
+  print('Hello world: ${coadingtest.calculate()}!');
+}
---
+++ dart/test/coadingtest_test.dart
@@ -0,0 +1,8 @@
+import 'package:coadingtest/coadingtest.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
